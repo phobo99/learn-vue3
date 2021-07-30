4fa89dd0b652094da42c6f8766308f58b66a3b30
phobo99 <phobotkcb@gmail.com>
2021-07-30 21:12:22 +0700

    [Ex4] List Rendering

---
+++ Intro-to-Vue3/04-List-Rendering/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inventory: 10,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2234,color:'green'},
+                {id:2235,color:'blue'},
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+        }
+    }
+})
