874768f3b8de7967bdc5b11f6f011955e0a84cfa
phobo99 <phobotkcb@gmail.com>
2021-07-30 21:12:40 +0700

    [Ex5] Event Handling

---
+++ Intro-to-Vue3/05-Event-Handling/main.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+    },
+    methods: {
+        addToCard() {
+            this.cart++
+        },
+        decreToCard() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        }
+        ,
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
