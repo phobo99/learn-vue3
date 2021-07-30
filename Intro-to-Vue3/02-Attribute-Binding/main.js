daa64b91af08f98bb1994d999b6a7ca03f54c0dc
phobo99 <phobotkcb@gmail.com>
2021-07-30 21:11:54 +0700

    [Ex2] Attribute Binding

---
+++ Intro-to-Vue3/02-Attribute-Binding/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            alt:'Sock blue',
+            image:'./assets/images/socks_green.jpg',
+            url: 'https://github.com/phobo99'
+        }
+    }
+})
