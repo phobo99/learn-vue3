453c4e170bec0676e04c3b2e056ec681ba75d72c
phobo99 <phobotkcb@gmail.com>
2021-08-04 00:08:28 +0700

    fix

--- TECHMASTER/hello-world/src/App.vue
+++ TECHMASTER/hello-world/src/App.vue
@@ -4,11 +4,74 @@
   </div> -->
 
   <div class="container">
-    <h1 class="text-center">{{ brand }}</h1>
-    <div class="d-flex justify-content-center mt-50 mb-50">
+    <div class="row justify-content-between">
+      <h1>{{ brand }}</h1>
+      <a href="#" class="btn btn-info btn-lg">
+        <span class="fa fa-shopping-cart"></span> {{ count }}
+      </a>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-12 card-margin">
+        <div class="card search-form">
+          <div class="card-body p-0">
+            <form id="search-form">
+              <div class="row">
+                <div class="col-12">
+                  <div class="row no-gutters">
+                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 ">
+                      <select
+                        class="form-control"
+                        id="exampleFormControlSelect1" 
+                      >
+                        <option hidden>Sort by...</option>
+                        <option>Feature</option>
+                        <option>Price: High to Low</option>
+                        <option>Price: Low to High</option>
+                        <option>A - Z</option>
+                        <option>Z - A</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-8 col-md-6 col-sm-12 p-0">
+                      <input
+                        type="text"
+                        placeholder="Search..."
+                        class="form-control"
+                        id="search"
+                        name="search"
+                      />
+                    </div>
+                    <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                      <button type="submit" class="btn btn-base">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="feather feather-search"
+                        >
+                          <circle cx="11" cy="11" r="8"></circle>
+                          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-center mt-20 mb-50">
       <div class="row">
         <div
-          class="col-md-4 mt-2"
+          class="col-md-4 mt-3 mb-2"
           v-for="product in products"
           :key="product.id"
         >
@@ -33,6 +96,9 @@
               <h3 class="mb-0 font-weight-semibold">
                 Price: ${{ product.price }}
               </h3>
+              <button type="button" class="btn bg-cart" @click="count += 1">
+                Add to cart
+              </button>
             </div>
           </div>
         </div>
@@ -47,23 +113,24 @@ export default {
   data() {
     return {
       brand: "AppleStore",
+      count: 0,
       products: [
         {
           id: 1,
           image: require("@/assets/iphone-x.jpg"),
-          name: "Iphone XSMax",
+          name: "Iphone XSMax 64GB",
           price: 500,
         },
         {
           id: 2,
           image: require("@/assets/iphone-11.jpg"),
-          name: "Iphone 11 Pro Max",
+          name: "Iphone 11 Pro Max 64GB",
           price: 1000,
         },
         {
           id: 3,
           image: require("@/assets/iphone-12.jpg"),
-          name: "Iphone 12 Pro Max",
+          name: "Iphone 12 Pro Max 128GB",
           price: 1500,
         },
         {
@@ -89,3 +156,36 @@ export default {
   },
 };
 </script>
+<style lang="css">
+body {
+  background: linear-gradient(to right, #c04848, #480048);
+  min-height: 100vh;
+}
+h1 {
+  color: #fff;
+}
+.bg-cart {
+  margin-top: 30px;
+  background-color: orange;
+  color: #fff;
+}
+
+.bg-cart:hover {
+  color: #fff;
+  opacity: .8;
+}
+
+.bg-buy {
+  background-color: green;
+  color: #fff;
+  padding-right: 29px;
+}
+
+.bg-buy:hover {
+  opacity: .8;
+  color: #fff;
+}
+.mt-50 {
+  margin-top: 100px;
+}
+</style>
